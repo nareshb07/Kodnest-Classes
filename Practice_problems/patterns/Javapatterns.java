a0d4d76c2770a852ecaa7c25370c96665b307c8b
nareshb07 <k.nareshb0077@gmail.com>
2025-01-10 18:31:01 +0530

    added day04

--- Practice_problems/patterns/Javapatterns.java
+++ Practice_problems/patterns/Javapatterns.java
@@ -1,13 +1,106 @@
-// first pattern in python
 
-//             * * * * *
-//             * * * * *
-//             * * * * *
-//             * * * * *
-//             * * * * *
 
 class JavaPatterns {
 
+    // Sixth pattern in python
+
+    // * * * * *
+    // * * * *
+    // * * *
+    // * *
+    // *
+
+    static void pattern5(int rows){
+        System.out.println("Printing pattern 5");
+        for(int i = rows;i>=1; i--) {
+            for (int j = 1; j <= i; j++){
+                System.out.print("* ");
+            }
+        System.out.println();
+        }
+    }
+
+    // Fifth pattern in python
+
+    // 1
+    // 2 2
+    // 3 3 3
+    // 4 4 4 4
+    // 5 5 5 5 5
+
+    static void pattern4(int rows){
+        for(int i = 1; i<=rows; i++) {
+            for (int j = 1; j <= i; j++){
+                System.out.print(i + " ");
+            }
+        System.out.println();
+        }
+    }
+
+            // Fourth pattern in python
+
+            //  1
+            //  1 2
+            //  1 2 3
+            //  1 2 3 4
+            //  1 2 3 4 5
+
+    static void pattern3(int rows) {
+        for(int i = 1; i<=rows; i++) {
+            for (int j = 1; j <= i; j++){
+                System.out.print(j + " ");
+            }
+        System.out.println();
+        }
+    }
+
+
+    // Third pattern in python
+
+    // *
+    // * *
+    // * * *
+    // * * * *
+    // * * * * *
+
+
+    static void pattern2(int rows) {
+        for (int i = 1; i<=rows;i++ ) {
+            for (int j = 1; j<=i ; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+    }
+
+            // Second pattern in python
+
+            // 1 1 1 1 1 
+            // 2 2 2 2 2
+            // 3 3 3 3 3
+            // 4 4 4 4 4
+
+
+    static void pattern1(int rows){
+
+        for (int i = 1 ; i<=rows; i++) {
+            
+            for(int j = 1; j <= rows; j++) {
+                System.out.print(i + " ");
+            }
+        System.out.println();
+        }
+
+    }
+
+    // first pattern in python
+
+//             * * * * *
+//             * * * * *
+//             * * * * *
+//             * * * * *
+//             * * * * *
     static void pattern(int rows){
 
         for (int i = 1 ; i<=rows; i++) {
@@ -17,12 +110,19 @@ class JavaPatterns {
             }
         System.out.println();
         }
-
     }
+
+
+
 
     public static void main(String[] args) {
        
        pattern(5);
+       pattern1(5);
+       pattern2(5);
+       pattern3(5);
+       pattern4(5);
+       pattern5(5);
 
     }
 
